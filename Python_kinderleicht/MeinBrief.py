bd64438ee59c2bb8d957f18f05365d1170530690
Thomas Hirmer <hi.thomas@gmx.de>
2015-04-07 18:08:14 +0200

    Mein Brief mit Umlauten

--- Python_kinderleicht/MeinBrief.py
+++ Python_kinderleicht/MeinBrief.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Leerzeichen = ' ' * 35
 print('%s Hinten Raus 12' % Leerzeichen)
 print('%s 11156 Osterschnarchheim' % Leerzeichen)
@@ -6,8 +7,8 @@ print()
 print('Sehr geehrte Damen und Herren')
 print()
 print('ich muss Ihnen berdauerlicherweise mitteilen, dass bei meinem')
-print('Toilettenhaeuschen einige Dachziegeln fehlen.')
-print('Ich glaube, dass der Sturm sie letzte Nacht heruntergweht hat.')
+print('Toilettenhäuschen einige Dachziegeln fehlen.')
+print('Ich glaube, dass der Sturm sie letzte Nacht heruntergeweht hat.')
 print()
-print('Mit freundlichen Gruessen')
+print('Mit freundlichen Grüßen')
 print('Max Maus')
